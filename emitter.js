c255fe6b3176e9b18abe50dce24f2b8d53ddee78
willlliammmsmithhh <67@felix-au.me>
2026-08-04 06:28:46 +0530

    Update emitter.js - Step 7

--- emitter.js
+++ emitter.js
@@ -1,2 +1,4 @@
 class Emitter {
     constructor() { this.events = {}; }
+    on(e, fn) { (this.events[e] = this.events[e] || []).push(fn); }
+    emit(e, d) { (this.events[e] || []).forEach(fn => fn(d)); }
